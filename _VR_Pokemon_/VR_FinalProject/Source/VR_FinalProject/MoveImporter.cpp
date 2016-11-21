3d6b7ac26f4141c650e677e0064dbb1ece3bc268
Brian Baron <brian.baron@mymail.champlain.edu>
2016-11-21 15:09:16 -0500

    Created new Unreal project

---
+++ _VR_Pokemon_/VR_FinalProject/Source/VR_FinalProject/MoveImporter.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VR_FinalProject.h"
+#include "MoveImporter.h"
+
+MoveImporter::MoveImporter()
+{
+}
+
+MoveImporter::~MoveImporter()
+{
+}
---
+++ _VR_Pokemon_/VR_FinalProject/Source/VR_FinalProject/MoveImporter.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class VR_FINALPROJECT_API MoveImporter
+{
+public:
+	MoveImporter();
+	~MoveImporter();
+};
